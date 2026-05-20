da8b4ba49ceda04cf7cd35b6f5577995a52645e1
Ashwani Kumar Singh <ashwanisingh2308@gmail.com>
2026-05-20 21:18:00 +0200

    load pdf and chunking

---
+++ app/components/pdf_loader.py
@@ -0,0 +1,50 @@
+import os
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+from app.config.config import DATA_PATH, CHUNK_SIZE, CHUNK_OVERLAP
+
+logger = get_logger(__name__)
+
+def load_pdf_files():
+    try:
+        if not os.path.exists(DATA_PATH):
+            raise CustomException("Data path doesn't exist.")
+        
+        logger.info(f"Loading files from {DATA_PATH}")
+
+        loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
+        documents = loader.load()
+
+        if not documents:
+            logger.warning("No pdfs were found")
+        else:
+            logger.info(f"Successfully loaded {len(documents)} documents")
+        
+        return documents
+    
+    except Exception as e:
+        error_message = CustomException("Failed to load PDF", e)
+        logger.error(str(error_message))
+        return []
+    
+
+def create_text_chunks(documents):
+    try:
+        if not documents:
+            raise CustomException("No Documents found")
+        
+        logger.info(f"Splitting {len(documents)} Documents")
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+        chunks = text_splitter.split_documents(documents=documents)
+        logger.info(f"Generated {len(chunks)} text chunks")
+        return chunks
+    
+    except Exception as e:
+        error_message = CustomException("Not able to create chunks")
+        logger.error(str(error_message))
+
